7d34933dcfe136291a384956c29cf367ed1a3f6b
Joshua Berliner <joshuaberliner@gmail.com>
2021-10-09 12:30:47 -0400

    commit tree ef3aa614ab5844e2687e993f211a881a3626cc11

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driverArray, string){
+
+    return driverArray.filter(o => o.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(driverArray, string){
+    return driverArray.filter(o => o.slice(0,string.length) === string);
+}
+
+function matchName(driverArray, string){
+    return driverArray.filter(o => o.name === string);
+}
